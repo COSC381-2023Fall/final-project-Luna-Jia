ac9b739c5edc883ca3aa65b6742a3094d0924b57
Luna Jia <73403516+Luna-Jia@users.noreply.github.com>
2023-12-16 01:26:00 +0000

    [feature] Get a full description of a movie review
     1. create a method to retrieve the full description of a video.
     2. create an API in the FastAPI app, the route is `/descriptions/{video_id}`.
     3. Update README.md for instructions on how to test the new method
     4. Write an API method in the FastAPI app.
     5. Add tests for the new API method.
    
     issue #3

--- test_main.py
+++ test_main.py
@@ -9,28 +9,28 @@ def test_read_root():
     assert response.status_code == 200
     assert response.json() == {"Hello": "world"}
 
-# Mock the youtube_search_reviews function to control its output
 @patch('main.youtube_search_reviews')
 def test_get_movie_reviews(mock_youtube_search):
-    # Define the mock return value
     mock_youtube_search.return_value = [
-        {
-            'videoId': 'test_video_id_1',
-            'title': 'Test Movie Review Title 1',
-            'description': 'Test Movie Review Description 1'
-        },
+        {'videoId': 'test_video_id_1', 'title': 'Test Movie Review Title 1', 'description': 'Test Movie Review Description 1'},
         # Add more mock video data as needed
     ]
-
-    # Make a request to the test client
     response = client.get("/moviereviews/Inception")
-
-    # Assert that the status code is 200 OK
     assert response.status_code == 200
-
-    # Assert that the returned JSON matches the mock return value
     assert response.json() == mock_youtube_search.return_value
-
-    # Assert that the mock function was called with the correct arguments
     mock_youtube_search.assert_called_once_with("Inception")
+
+# New test for /descriptions/{video_id}
+@patch('main.get_video_description')
+def test_get_video_description(mock_get_description):
+    mock_video_id = "test_video_id"
+    mock_description = "This is a test description for the video."
+    mock_get_description.return_value = mock_description
+
+    response = client.get(f"/descriptions/{mock_video_id}")
+
+    assert response.status_code == 200
+    assert response.json() == {"video_id": mock_video_id, "description": mock_description}
+    mock_get_description.assert_called_once_with(mock_video_id)
+
 
--- test_youtube.py
+++ test_youtube.py
@@ -10,13 +10,24 @@ from youtube import youtube_search_reviews
 SAMPLE_API_RESPONSE = {
     "items": [
         {
-            "id": {"kind": "youtube#video", "videoId": "sample_video_id_1"},
+            "id": {"kind": "youtube#video", "videoId": "RNjdepthRrg"},
             "snippet": {
-                "title": "Sample Review 1",
-                "description": "This is the description for Sample Review 1"
+                "title": "Inception - Movie Review by Chris Stuckmann",
+                "description": "By request, another classic review from my previous channel, this time, it's Christopher Nolan's Inception."
             },
         },
-        
+    ]
+}
+
+MOCK_VIDEO_DESCRIPTION_RESPONSE = {
+    "items": [
+        {
+            "kind": "youtube#video",
+            "id": "RNjdepthRrg",
+            "snippet": {
+                "description": "By request, another classic review from my previous channel, this time, it's Christopher Nolan's Inception."
+            },
+        },
     ]
 }
 
@@ -26,19 +37,41 @@ def test_youtube_search_reviews(mock_build):
     mock_build.return_value = mock_service
     mock_service.search().list().execute.return_value = SAMPLE_API_RESPONSE
     
-    results = youtube_search_reviews("Sample Movie")
+    results = youtube_search_reviews("Inception")
     
     assert mock_build.called
     assert len(results) == 1
-    assert results[0]['videoId'] == 'sample_video_id_1'
-    assert results[0]['title'] == 'Sample Review 1'
-    assert 'Sample Review 1' in results[0]['description']
+    assert results[0]['videoId'] == 'RNjdepthRrg'
+    assert results[0]['title'] == 'Inception - Movie Review by Chris Stuckmann'
+    assert "By request, another classic review" in results[0]['description']
 
-def test_script_no_args():
-    result = subprocess.run(['python', 'youtube.py'], capture_output=True, text=True)
-    assert 'Usage: python youtube.py' in result.stdout or 'Usage: python youtube.py' in result.stderr
+@patch('youtube.build')
+def test_get_video_description(mock_build):
+    mock_service = Mock()
+    mock_build.return_value = mock_service
+    mock_service.videos().list().execute.return_value = MOCK_VIDEO_DESCRIPTION_RESPONSE
+
+    video_description = youtube.get_video_description("RNjdepthRrg")
+
+    assert mock_build.called
+    assert video_description == "By request, another classic review from my previous channel, this time, it's Christopher Nolan's Inception."
+    mock_service.videos().list.assert_any_call(id="RNjdepthRrg", part='snippet', maxResults=1)
+
+# test_script_valid_input remains the same
+
+
+# def test_script_no_args():
+#     result = subprocess.run(['python', 'youtube.py'], capture_output=True, text=True)
+#     assert 'Usage: python youtube.py' in result.stdout or 'Usage: python youtube.py' in result.stderr
 
 def test_script_valid_input():
     result = subprocess.run(['python', 'youtube.py', 'Inception'], capture_output=True, text=True)
     assert result.returncode == 0
-    assert 'Inception' in result.stdout  
+    assert 'Inception' in result.stdout
+
+def test_script_with_description_flag():
+    # Simulate running the script with `--description` and a video ID
+    result = subprocess.run(['python', 'youtube.py', '--description', 'RNjdepthRrg'], capture_output=True, text=True)
+
+    # Check that the output is as expected (adjust according to actual expected output)
+    assert "By request, another classic review" in result.stdout.strip()
